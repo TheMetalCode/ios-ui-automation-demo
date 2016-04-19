ee3dbbf71d66a52455aed489a278b7ae25d37172
Jason Hagglund <hagglundized@gmail.com>
2016-04-19 11:00:23 -0700

    Tab bar controller

--- ios-ui-automation-demo/AppDelegate.swift
+++ ios-ui-automation-demo/AppDelegate.swift
@@ -35,6 +35,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        let tabBarController = self.window?.rootViewController as! UITabBarController
+        tabBarController.selectedIndex = 0
     }
 
     func applicationWillTerminate(application: UIApplication) {
---
+++ ios-ui-automation-demo/PhotoTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoTableViewController.swift
+//  ios-ui-automation-demo
+//
+//  Created by Jason Hagglund on 4/19/16.
+//  Copyright © 2016 Jason Hagglund. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewController: UIViewController {
+
+}
